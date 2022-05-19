65d76664cbae0436fcd6b001b226927830b0a129
mateugazi <mateugazi@protonmail.com>
2022-05-19 18:36:11 +0200

    Added upper, lower and capitalize text transformations

---
+++ src/main/java/pl/put/poznan/transformer/transformation/CapitalizeTextTransformation.java
@@ -0,0 +1,20 @@
+package pl.put.poznan.transformer.transformation;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class CapitalizeTextTransformation implements TextTransformation{
+    public static final String NAME = "capitalize";
+
+    @Override
+    public String apply(String s) {
+        if ("".equals(s)) {
+            return s;
+        }
+
+        return Arrays.asList(s.split(" "))
+                .stream()
+                .map(el -> el.substring(0, 1).toUpperCase() + el.substring(1))
+                .collect(Collectors.joining(" "));
+    }
+}
