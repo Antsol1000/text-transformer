361cc723ad6226ae223b10a555fb9297aa8f6284
ja-nina <81194757+ja-nina@users.noreply.github.com>
2022-05-22 18:13:43 +0200

    added first javadocs (#20)

--- src/main/java/pl/put/poznan/transformer/transformation/LatexTextTransformation.java
+++ src/main/java/pl/put/poznan/transformer/transformation/LatexTextTransformation.java
@@ -1,5 +1,11 @@
 package pl.put.poznan.transformer.transformation;
 
+/**
+ * Class proving transformation of & and $ into latex format
+ * @author Nina Zukowska
+ * @version 1.6
+ */
+
 public class LatexTextTransformation extends TextTransformation {
     public static final String NAME = "latex";
 
@@ -17,6 +23,11 @@ public class LatexTextTransformation extends TextTransformation {
         return s1.replace("&", "\\&");
     }
 
+    /**
+     * Method applying the transformation
+     * @param text The text to which transformation will be applied
+     * @return transformed text
+     */
     @Override
     public String transform(final String text) {
         return latex(super.transform(text));
